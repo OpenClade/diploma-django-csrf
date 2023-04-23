7710b96fd85da75b037cd2f913386c9403b00e20
Serikzhan Kuanyshev <kuanyshev@mail.ru>
2023-04-23 22:50:25 +0600

    feat (django): add html check file

--- csrftest/settings.py
+++ csrftest/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myapp',
 ]
+"""
+/home/clevtech/Documents/contest/diploma/csrf/login_attack.html
+"""
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
